02f13184f3ec624154bfbd4f53f1e3f9ebd17eff
komenouka <nekozuki_313@yahoo.co.jp>
2020-08-19 20:09:35 +0900

    パス修正

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ $(function() {
 
     // 曲数分の配列準備
     var Arrayjoy = new Array(joyTracks);
-    for( let s=0; s < (joyTracks - 1); s++ ){
+    for (let s = 0; s < (joyTracks - 1); s++) {
         Arrayjoy[s] = s;
     }
     var Arraydam = new Array(damTracks);
-    for( let s=0; s < (damTracks - 1); s++ ){
+    for (let s = 0; s < (damTracks - 1); s++) {
         Arraydam[s] = s;
     }
 
@@ -29,7 +29,7 @@ $(function() {
     shuffle(Arraydam);
 
     // リセットかける
-    $('#reset').on('click',function(){
+    $('#reset').on('click', function() {
         // 入れ替え
         shuffle(Arrayjoy);
         shuffle(Arraydam);
@@ -39,57 +39,56 @@ $(function() {
     });
 
     // ランダム選曲
-    $('#submit').on('click',function(){
+    $('#submit').on('click', function() {
         var i = 0;
 
         var model = $('input[name=model]:checked').val();
         switch (model) {
             // JOYSOUND
             case 'joy':
-            var modelJson = $.getJSON('data/joy.json');
-            var modelName = 'JOYSOUND';
-            // 全曲で終わったら
-            if( countjoy == joyTracks ){
-                shuffle(Arrayjoy);
-                countjoy = 0;
-            }
-            i = Arrayjoy[countjoy];
-            countjoy++;
-            $('.tx-remaining').text("残り" + (joyTracks - countjoy) + "曲");
-            break;
+                var modelJson = $.getJSON('./data/joy.json');
+                var modelName = 'JOYSOUND';
+                // 全曲で終わったら
+                if (countjoy == joyTracks) {
+                    shuffle(Arrayjoy);
+                    countjoy = 0;
+                }
+                i = Arrayjoy[countjoy];
+                countjoy++;
+                $('.tx-remaining').text("残り" + (joyTracks - countjoy) + "曲");
+                break;
 
-            // DAM
+                // DAM
             case 'dam':
-            var modelJson = $.getJSON('data/dam.json');
-            var modelName = 'DAM';
-            // 全曲で終わったら
-            if( countdam == damTracks ){
-                shuffle(Arraydam);
-                countdam = 0;
-            }
-            i = Arraydam[countdam];
-            countdam++;
-            $('.tx-remaining').text("残り" + (damTracks - countdam) + "曲");
-            break;
+                var modelJson = $.getJSON('./data/dam.json');
+                var modelName = 'DAM';
+                // 全曲で終わったら
+                if (countdam == damTracks) {
+                    shuffle(Arraydam);
+                    countdam = 0;
+                }
+                i = Arraydam[countdam];
+                countdam++;
+                $('.tx-remaining').text("残り" + (damTracks - countdam) + "曲");
+                break;
         }
 
         modelJson // json読み込み開始
-        .done(function(data){ // jsonの読み込みに成功した時
+            .done(function(data) { // jsonの読み込みに成功した時
             //　機種名表示
             $('.tx-model-result').text(modelName);
             // 曲番号
             $('.tx-number-result').text(data[i].number);
 
             // チェック入ってたら曲名も表示
-            if($('#song-title-check').prop('checked') == true) {
+            if ($('#song-title-check').prop('checked') == true) {
                 $('.tx-title-result').text(data[i].title);
-                var tweet = '歌う曲は「' + data[i].title　+ '(' + data[i].number + ')」です';
-            }
-            else {
+                var tweet = '歌う曲は「' + data[i].title　 + '(' + data[i].number + ')」です';
+            } else {
                 $('.tx-title-result').text('〇〇〇〇');
                 var tweet = '歌う曲の番号は「' + data[i].number + '」です';
             }
-        $('.btn-share-twitter').attr('href', 'http://twitter.com/intent/tweet?url=http://tsukiyoni.oteage.net/&text=' + tweet + '&hashtags=sideMランダム選曲ジェネレーター');
+            $('.btn-share-twitter').attr('href', 'http://twitter.com/intent/tweet?url=http://tsukiyoni.oteage.net/&text=' + tweet + '&hashtags=sideMランダム選曲ジェネレーター');
         })
     });
 
@@ -100,7 +99,7 @@ $(function() {
     var topBtn = $('#page-top');
     topBtn.hide();
     //スクロールが100に達したらボタン表示
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             topBtn.fadeIn();
         } else {
@@ -108,7 +107,7 @@ $(function() {
         }
     });
     //スクロールしてトップ
-    topBtn.click(function () {
+    topBtn.click(function() {
         $('body,html').animate({
             scrollTop: 0
         }, 500);
@@ -116,22 +115,23 @@ $(function() {
     });
 
     // もっとみるボタン
-    $(window).on('load', function(){
-        listShow('.box-news ul','5');//デフォルトで表示される数を指定
-        $("#js-btn-wrap .btn").on("click",function(){
-            listShow('.box-news ul','10','#js-btn-wrap');//ボタンクリック後表示される数を指定
+    $(window).on('load', function() {
+        listShow('.box-news ul', '5'); //デフォルトで表示される数を指定
+        $("#js-btn-wrap .btn").on("click", function() {
+            listShow('.box-news ul', '10', '#js-btn-wrap'); //ボタンクリック後表示される数を指定
         })
     });
-    function listShow(id,cnt,btn){
-        var girls_list   = id + " .js-load:not(.active)";
+
+    function listShow(id, cnt, btn) {
+        var girls_list = id + " .js-load:not(.active)";
         var girls_length = $(girls_list).length;
         var girls_total_cnt;
-        if( cnt < girls_length ){
+        if (cnt < girls_length) {
             girls_total_cnt = cnt;
         } else {
             girls_total_cnt = girls_length;
             $(btn).hide();
         }
-        $(girls_list + ":lt(" + girls_total_cnt +")").addClass("active");
+        $(girls_list + ":lt(" + girls_total_cnt + ")").addClass("active");
     }
 });
